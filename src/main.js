c19b744f760316818278d489f2645c65be206cfc
DEADushka108 <cloxx108@gmail.com>
2020-07-30 14:20:15 +0500

    Добавляет описание функций

--- src/main.js
+++ src/main.js
@@ -15,23 +15,50 @@ const headerElement = document.querySelector(`.header`);
 const mainElement = document.querySelector(`.main`);
 const footerElement = document.querySelector(`.footer`);
 
+/**
+ * Генерирует елемент из шаблона
+ * @param {element} контейнер для генерации
+ * @param {str} генерируемый шаблон
+ * @param {str} положение для генерации
+ */
 const renderElement = (container, template, position = `beforeend`) => {
   container.insertAdjacentHTML(position, template);
 };
 
+/**
+ * Создает элемент из шаблона
+ * @param {str} шаблон
+ * 
+ * @return {Node} элемент DOM'a
+ */
 const createElement = (template) => {
   let element = document.createElement(`div`);
   element.innerHTML = template;
   return element.firstChild;
 };
+
+/**
+ * Устанавливает количество карточек для генерации
+ * @param {boolean} наличие модификатора
+ * 
+ * @return {int} колтчество карточек
+ */
 const setCount = (isExtra) => {
   return isExtra ? CARD_COUNT_EXTRA : CARD_COUNT;
 };
+
+/**
+ * Отрисовывает карточки согласно установленому количеству
+ * @param {boolean} наличие модификатора
+ * @param {element} контейнер для генерации карточек  
+ */
 const renderCards = (isExtra, container) => {
   let count = setCount(isExtra);
+
   for (let i = 0; i < count; i++) {
     renderElement(container, createFilmCardTemplate());
   }
+
 };
 
 renderElement(headerElement, createUserProfileTemplate());
@@ -41,14 +68,22 @@ renderElement(mainElement, createFilmsTemplate());
 
 const filmsElement = mainElement.querySelector(`.films`);
 
+/**
+ * Генерирует блок с элементами
+ * @param {element} контейнер  
+ * @param {str} заголовок блока 
+ * @param {str} модификатор блока 
+ */
 const renderFilmsBlock = (container, title, modifier) => {
   const containerListElement = createElement(createFilmsListTemplate(title, modifier));
   const containerElement = containerListElement.querySelector(`.films-list__container`);
   container.appendChild(containerListElement);
   renderCards(modifier, containerElement);
+  
   if (!modifier) {
     renderElement(containerListElement, createShowMoreBtnTemplate());
   }
+
 };
 
 renderFilmsBlock(filmsElement, `All movies. Upcoming`);
