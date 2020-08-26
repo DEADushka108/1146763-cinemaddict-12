eaca07bdcad1f7aa1d9ea55351598a1037d2898c
DEADushka108 <cloxx108@gmail.com>
2020-08-26 15:05:30 +0500

    Delete unnecessary components

--- src/components/no-films.js
+++
@@ -1,7 +0,0 @@
-import AbstractComponent from './abstract-component.js';
-
-export default class NoFilms extends AbstractComponent {
-  getTemplate() {
-    return `<h2 class="films-list__title">There are no movies in our database</h2>`;
-  }
-}
