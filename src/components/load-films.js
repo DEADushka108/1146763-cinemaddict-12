6715395b4f921b6d0d07a28eb4eec3764b94a071
DEADushka108 <cloxx108@gmail.com>
2020-08-30 00:36:44 +0500

    Delete unnecessary components

--- src/components/load-films.js
+++
@@ -1,7 +0,0 @@
-import AbstractComponent from './abstract-component.js';
-
-export default class LoadFilms extends AbstractComponent {
-  getTemplate() {
-    return `<h2 class="films-list__title">Loading...</h2>`;
-  }
-}
