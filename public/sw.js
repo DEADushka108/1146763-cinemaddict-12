7c85129dc485115f0a6d3f28aece654bda532065
DEADushka108 <cloxx108@gmail.com>
2020-09-03 16:11:39 +0500

    create cache assets for sw

--- public/sw.js
+++ public/sw.js
@@ -5,38 +5,40 @@ const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VER}`;
 const HTTP_STATUS_OK = 200;
 const RESPONSE_SAFE_TYPE = `basic`;
 
+const CASHED_ASSETS = [
+  `/`,
+  `/index.html`,
+  `/bundle.js`,
+  `/css/normalize.css`,
+  `/css/main.css`,
+  `/images/emoji/angry.png`,
+  `/images/emoji/puke.png`,
+  `/images/emoji/sleeping.png`,
+  `/images/emoji/smile.png`,
+  `/images/icons/icon-favorite-active.svg`,
+  `/images/icons/icon-favorite.svg`,
+  `/images/icons/icon-watched-active.svg`,
+  `/images/icons/icon-watched.svg`,
+  `/images/icons/icon-watchlist-active.svg`,
+  `/images/icons/icon-watchlist.svg`,
+  `/images/background.png`,
+  `/images/bitmap.png`,
+  `/images/bitmap@2x.png`,
+  `/images/bitmap@3x.png`,
+  `/images/posters/made-for-each-other.png`,
+  `/images/posters/popeye-meets-sinbad.png`,
+  `/images/posters/sagebrush-trail.jpg`,
+  `/images/posters/santa-claus-conquers-the-martians.jpg`,
+  `/images/posters/the-dance-of-life.jpg`,
+  `/images/posters/the-great-flamarion.jpg`,
+  `/images/posters/the-man-with-the-golden-arm.jpg`,
+];
+
 self.addEventListener(`install`, (evt) => {
   evt.waitUntil(
       caches.open(CACHE_NAME)
         .then((cache) => {
-          return cache.addAll([
-            `/`,
-            `/index.html`,
-            `/bundle.js`,
-            `/css/normalize.css`,
-            `/css/main.css`,
-            `/images/emoji/angry.png`,
-            `/images/emoji/puke.png`,
-            `/images/emoji/sleeping.png`,
-            `/images/emoji/smile.png`,
-            `/images/icons/icon-favorite-active.svg`,
-            `/images/icons/icon-favorite.svg`,
-            `/images/icons/icon-watched-active.svg`,
-            `/images/icons/icon-watched.svg`,
-            `/images/icons/icon-watchlist-active.svg`,
-            `/images/icons/icon-watchlist.svg`,
-            `/images/background.png`,
-            `/images/bitmap.png`,
-            `/images/bitmap@2x.png`,
-            `/images/bitmap@3x.png`,
-            `/images/posters/made-for-each-other.png`,
-            `/images/posters/popeye-meets-sinbad.png`,
-            `/images/posters/sagebrush-trail.jpg`,
-            `/images/posters/santa-claus-conquers-the-martians.jpg`,
-            `/images/posters/the-dance-of-life.jpg`,
-            `/images/posters/the-great-flamarion.jpg`,
-            `/images/posters/the-man-with-the-golden-arm.jpg`,
-          ]);
+          return cache.addAll(CASHED_ASSETS);
         })
   );
 });
